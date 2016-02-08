c6ea2d2f7a1677866e3d39ca95902d31df8dcfc6
avinash jaiswal <jaisavi25@gmail.com>
2016-02-08 20:58:50 +0530

    push

--- main/java/com/example/appinventiv/animation/MainActivity.java
+++ main/java/com/example/appinventiv/animation/MainActivity.java
@@ -27,6 +27,28 @@ private ImageView imageview;
             image.startAnimation(animation);
         }
 
+        public void zoom(View view){
+            ImageView image = (ImageView)findViewById(R.id.imageView);
+            Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom);
+            image.startAnimation(animation1);
+        }
 
 
+        public void blink(View view){
+            ImageView image = (ImageView)findViewById(R.id.imageView);
+            Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+            image.startAnimation(animation1);
+        }
+
+        public void move(View view){
+            ImageView image = (ImageView)findViewById(R.id.imageView);
+            Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+            image.startAnimation(animation1);
+        }
+
+        public void slide(View view){
+            ImageView image = (ImageView)findViewById(R.id.imageView);
+            Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide);
+            image.startAnimation(animation1);
+        }
 }
